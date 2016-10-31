0861dd775ee91c8efe1e63c72612231967002129
Diego <diego@DDIAS.local>
2016-10-31 18:55:48 -0200

    CORRECOES DE MENU MOBILE E DESENVOLVIMENTO DO FLEXBOX DA SECAO TRABALHOS

--- js/main.js
+++ js/main.js
@@ -103,52 +103,32 @@ $(".sidebar-nav li a").click(function(){
 });
 
 
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
 
-//MENU MOBILE 
-$(window).on("load resize",function(e) {
-  var more = document.getElementById("js-centered-more");
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
 
-  if ($(more).length > 0) {
-    var windowWidth = $(window).width();
-    var moreLeftSideToPageLeftSide = $(more).offset().left;
-    var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
 
-    if (moreLeftSideToPageRightSide < 330) {
-      $("#js-centered-more .submenu .submenu").removeClass("fly-out-right");
-      $("#js-centered-more .submenu .submenu").addClass("fly-out-left");
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
     }
-
-    if (moreLeftSideToPageRightSide > 330) {
-      $("#js-centered-more .submenu .submenu").removeClass("fly-out-left");
-      $("#js-centered-more .submenu .submenu").addClass("fly-out-right");
-    }
-  }
-
-  var menuToggle = $("#js-centered-navigation-mobile-menu").unbind();
-  $("#js-centered-navigation-menu").removeClass("show");
-
-  menuToggle.on("click", function(e) {
-    e.preventDefault();
-    $("#js-centered-navigation-menu").slideToggle(function(){
-      if($("#js-centered-navigation-menu").is(":hidden")) {
-        $("#js-centered-navigation-menu").removeAttr("style");
-      }
-    });
-  });
-
-      $(".centered-navigation ul li.nav-link a").click(function(){
-        $(".centered-navigation ul li.nav-link a div , .centered-navigation ul li.nav-link a div ").removeClass("hovered");
-        $(".centered-navigation ul li.nav-link a:focus div , .centered-navigation ul li.nav-link a:hover div ").toggleClass("hovered");
-            $("#js-centered-navigation-menu").slideToggle(function(){
-      if($("#js-centered-navigation-menu").is(":hidden")) {
-        $("#js-centered-navigation-menu").removeAttr("style");
-      }
-    });
-
-        
-    });
-}); 
-
-
-
-
+}
