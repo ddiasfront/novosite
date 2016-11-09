7378a10823920ff8aeaff8514eb5db07c69e7ff6
Diego <diego@DDIAS.local>
2016-11-09 17:06:42 -0200

    site tinindo e testado nos mobiles

--- js/main.js
+++ js/main.js
@@ -485,7 +485,17 @@ $('.demolead').click(function() {
 
    $( document ).ready(function() {
 
+        function hideURLbar() {
+          if (window.location.hash.indexOf('#') == -1) {
+            window.scrollTo(0, 1);
+          }
+        }
 
+        if (navigator.userAgent.indexOf('iPhone') != -1 || navigator.userAgent.indexOf('Android') != -1) {
+            addEventListener("load", function() {
+                    setTimeout(hideURLbar, 0);
+            }, false);
+        }
 
      $(window).scroll(function (argument) {
 
@@ -580,6 +590,8 @@ $('.demolead').click(function() {
     $(".contatolinkup").click(function() {
        scrollToAnchor('contatolinkdown');
     });
+
+
 
 
 
